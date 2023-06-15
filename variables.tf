e59075375d1c92e385ece71f3af3fa95e9612f0d
Manjunatha Gowda <34675068+manjubluemoon@users.noreply.github.com>
2023-06-15 08:40:34 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -20,10 +20,16 @@ variable "availability_zone" {
 
 variable "ami_id" {
   description = "AMI ID for the Kali Linux instance"
-  default     = "kali-last-snapshot-amd64-2023.2.0-804fcc46-63fc-4eb6-85a1-50e66d6c7215ami-0bc49507a10bfd74f"
+  default     = "ami-0bc49507a10bfd74f"
 }
 
 variable "key_pair_name" {
   description = "Name of the SSH key pair"
   default     = "kali-key"
+}
+
+variable "SSH_PRIVATE_KEY" {
+  description = "Private key for SSH access"
+  type        = string
+  default     = "default_ssh_private_key"
 }
